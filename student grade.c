1294df1518f6931321d780fec582e7e56908ae5c
SBhuvana8050 <sbhuvana8050@gmail.com>
2025-11-10 10:19:00 +0530

    Add files via upload

---
+++ student grade.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+int main()
+{
+    float m1,m2,m3,total,average;
+char grade;
+printf("enter the marks of three subjects(out of 100):");
+scanf("%f%f%f",&m1,&m1,&m3);
+total=m1+m2+m3;
+average=total/3;
+printf("\n Total marks=%.2f",total);
+printf("\n Average marks=%.2f",average);
+if(average>=90)
+    grade='A';
+else if(average>=80)
+    grade='B';
+    else if(average>=70)
+        grade='C';
+else if(average>=60)
+    grade='D';
+else if(average>=50)
+    grade='E';
+else
+    grade='F';
+printf("\n Grade=%c",grade);
+if(average>=50)
+    printf("\n Ressult:pass\n");
+else
+    printf("\n Result:fail\n");
+return 0;
+}
